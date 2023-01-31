4f06438046fa0a6e6c7eab11d8f157c1bcbdd166
Amir Nazari <amir@nazari.sydney>
2023-02-01 08:42:16 +1100

    Added updated text for the packages

--- src/components/main/services/main.svelte
+++ src/components/main/services/main.svelte
@@ -8,38 +8,68 @@
 	};
 	let services: Service[] = [
 		{
-			name: 'Maintenance',
-			time: '2 - 3 Hours',
-			paragraph:
-				'High pressure rinse, foam and two bucket method wash. wheels, tyres and wheel arches will be cleaned vehicle will be dried with a twisted loop drying towel interior wiped down, vacuumed and glass cleaned',
+			name: "Pre-sale Detail",
+			time: '1 Day',
+			paragraph:"",
 			image: 'service-images/image-0.jpeg'
 		},
 		{
-			name: 'Paint Correction',
+			name: 'Standard',
 			time: '1 - 2 Days',
 			paragraph:
-				"Paint correction includes a detailed exterior wash using two buckets, a high-pressure rinse, and foam. The vehicle will also receive a decontamination, iron removal/clay treatment to remove any embedded contaminants that can't be removed with a regular wash. Contactless drying to prevent any chances of scratches, and single or multiple-stage paint correction.",
+				'Pre-foam\n' +
+				'Wheels and wheel arches cleaned\n' +
+				'Two bucket wash\n' +
+				'Clay bar and decontamination\n' +
+				'Microfiber and blow dry\n' +
+				'Single stage paint correction\n' +
+				'Two-year Gyeon coating\n' +
+				'Basic interior detailing\n' +
+				'Interior vacuum\n' +
+				'Dash and trims sanitized\n' +
+				'Glass cleaning\n',
+
 			image: 'service-images/image-1.jpeg'
 		},
 		{
-			name: 'Paint Protection',
+			name: 'Premium',
 			time: '2 - 3 Days',
 			paragraph:
-				'Paint protection will include a paint correction whether its a single stage paint correction which can remove 55-75% of imperfections or two stage correction which removes 75-90%. Application of Gyeon 3 year ceramic coating.',
+				"Pre-foam\n" +
+				"Wheels and wheel arches cleaned\n" +
+				"Two bucket wash\n" +
+				"Clay bar and decontamination\n" +
+				"Microfiber and blow dry\n" +
+				"Multi-stage paint correction\n" +
+				"Four-year Gyeon coating\n" +
+				"Basic interior detailing\n" +
+				"Interior vacuum\n" +
+				"Glass coating\n",
 			image: 'service-images/image-2.jpeg'
 		},
 		{
-			name: 'Pre-sale Detail',
-			time: '1 - 2 Days',
+			name: 'Presidential',
+			time: '2 - 3 Days',
 			paragraph:
-				'Pre-sale detail includes: High pressure rinse, foam and two bucket method wash. Wheels, tyres, and wheel arches will be cleaned. Vehicle will be dried with a twisted loop drying towel. Interior wiped down and steamed, thoroughly vacuumed, leather seats cleaned & conditioned.',
+				"Pre-foam\n" +
+				"Wheels and wheel arches cleaned\n" +
+				"Two bucket wash\n" +
+				"Clay bar and decontamination\n" +
+				"Microfiber and blow dry\n" +
+				"Multi-stage paint correction\n" +
+				"Dual layer Gyeon coating\n" +
+				"Basic interior detailing\n" +
+				"Interior vacuum\n" +
+				"Glass coating\n" +
+				"Leather coating\n" +
+				"Wheel coating\n",
 			image: 'service-images/image-3.jpeg'
 		}
 	];
 </script>
 
 <div class="p-4 bg-black text-white">
-	<h3 class="text-3xl font-bold mb-10">Services</h3>
+	<h3 class="text-3xl font-bold mb-10">Packages</h3>
 	<div class="flex">
 		<ul class="md:flex-1 md:w-[50%] space-y-16">
 			{#each services as service}
--- src/components/main/services/serviceBlock.svelte
+++ src/components/main/services/serviceBlock.svelte
@@ -3,13 +3,19 @@
 	export let time: string;
 	export let paragraph: string;
 	export let image: string;
+	const paragraphsArray = paragraph.split('\n');
+
 </script>
 
 <li class="">
 	<div class="flex flex-col ">
 		<h4 class="text-center font-bold text-lg">{name}</h4>
 		<h5 class="text-center font-thin text-xs -mt-1">{time}</h5>
-		<p class="text-sm text-center font-normal px-4">{paragraph}</p>
+		<ul class="text-lg text-center font-normal px-4 space-y-2">
+			{#each paragraphsArray as p}
+				<li>{p}</li>
+			{/each}
+		</ul>
 		<img class="h-48 object-cover mt-3" src={image} alt="" />
 		<div
 			class="h-12 px-10 border-[1px] border-white cursor-pointer active:border-blue-500 transition-all ease-in-out">
