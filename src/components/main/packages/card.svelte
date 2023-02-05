24c9b649c22775c75c8c70e0f7455882ce32bae3
Amir Nazari <amir@nazari.sydney>
2023-02-05 15:00:21 +1100

    Added cards for Pre-sale and Standard packages.

---
+++ src/components/main/packages/card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  type Package = {
+    title: string,
+    duration: string,
+    price: string,
+    services: string[]
+  }
+  export let p: Package;
+</script>
+<div class="bg-[#141414] h-[618px] w-full md:h-[892px] md:w-[250px] flex-1 md:h-full md:p-8">
+  <div class=" my-4 text-center text-4xl font-bold">{p.title}</div>
+  <div class=" text-center text-sm">
+    <p class="text-xs mb-2">{p.duration}</p>
+    <p class="text-xs mb-2">{p.price}</p>
+    <p class="text-xs">Prices vary depending on size and condition of vehicle</p>
+  </div>
+  <div class=" mt-6">
+    <ul class="flex flex-col space-y-4 px-4">
+      {#each p.services as service}
+        <li class="flex items-center">
+          <img src="checkbox.svg" alt="" class="h-6">
+          <p class="text-xs ml-4">{service}</p>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
---
+++ src/components/main/packages/main.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import Card from "./card.svelte";
+  type Package = {
+    title: string,
+    duration: string,
+    price: string,
+    services: string[]
+  }
+  const preSale: Package = {
+    title: "Pre-sale",
+    duration: "1 Day",
+    price: "Starting from $500",
+    services: [
+      "Wheels and arches cleaned",
+      "Engine bay detail",
+      "High pressure rinse & foam with two bucket wash",
+      "Contactless heated blow dry to prevent any further scratches",
+      "Gloss enhancement polish",
+      "Hydrophobic spray sealant applied",
+      "Tyres dressed",
+      "Carpet cleaned and sanitised",
+      "Rubber floor mats pressure cleaned and sanitised",
+      "Leather seats cleaned & conditioned",
+      "Interior glass and windshield cleaned",
+      "Foot pedals cleaned and sanitised"
+    ]
+  }
+  const standard: Package = {
+    title: "Standard",
+    duration: "1 - 2 Days",
+    price: "Starting from $800",
+    services: [
+      "Wheels and arches cleaned",
+      "High pressure rinse & foam with two bucket wash",
+      "Decontamination and clay bar treatment",
+      "Contactless heated blow dry to prevent any further scratches",
+      "Single stage paint correction",
+      "Application of Gyeon ONE EVO ( two year year coating)",
+      "Tyres dressed",
+      "Carpet cleaned and sanitised",
+      "Rubber floor mats pressure cleaned",
+      "Interior glass and windshield cleaned",
+      "Foot pedals cleaned and sanitised"
+    ]
+  }
+
+</script>
+
+<div class="flex flex-col space-y-6 p-4 md:justify-evenly md:flex-row md:space-y-0 md:space-x-8">
+  <Card p={preSale}/>
+  <Card p={standard}/>
+  <Card p={preSale}/>
+</div>
